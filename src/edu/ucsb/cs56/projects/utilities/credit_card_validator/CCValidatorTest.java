ae7ec8ad5b9a0bd503bf6d2a554b703457e54b7f
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-02-25 10:28:47 -0800

    changed CCValidatorTest to reflect change from CheckCard method of type String to isValid method of type boolean

--- src/edu/ucsb/cs56/projects/utilities/credit_card_validator/CCValidatorTest.java
+++ src/edu/ucsb/cs56/projects/utilities/credit_card_validator/CCValidatorTest.java
@@ -16,48 +16,48 @@ public class CCValidatorTest {
        tests if the outcome of RandomCardGen is valid
     */
     @Test public void testRandomCardGen() {
-	String result = null;
+	Boolean result;
 	
 	String testCard = RandomCardGen.RandomCard();
-	result = CCValidator.CheckCard(testCard);
-	assertEquals("approved", result);
+	result = CCValidator.isValid(testCard);
+	assertEquals(true, result);
     }
     
     /**
-       test CheckCard Method in CCValidator class
+       test isValid Method in CCValidator class
      */
-    @Test public void testCheckCard() {
-	String result = null;
+    @Test public void testisValid() {
+	Boolean result;
 
 	String testCard2 = "5000000000000000";
-	result = CCValidator.CheckCard(testCard2);
-	assertEquals("denied", result);
+	result = CCValidator.isValid(testCard2);
+	assertEquals(false, result);
 
 	String testCard3 = "4200000000000000";
-	result = CCValidator.CheckCard(testCard3);
-	assertEquals("approved", result);
+	result = CCValidator.isValid(testCard3);
+	assertEquals(true, result);
     }
 
     /**
-       test CheckCard Method with card number contain spaces and "-"
+       test isValid Method with card number contain spaces and "-"
      */
     @Test public void testCardWithSpace(){
-	String result = null;
+	Boolean result = null;
 
 	String testCard4 = "5000 0000 0000 0000";
-	result = CCValidator.CheckCard(testCard4);
-	assertEquals("denied", result);
+	result = CCValidator.isValid(testCard4);
+	assertEquals(false, result);
 
 	String testCard5 = "4200 0000 0000 0000";
-	result = CCValidator.CheckCard(testCard5);	
-	assertEquals("approved", result);
+	result = CCValidator.isValid(testCard5);	
+	assertEquals(true, result);
 
 	String testCard6 = "5000-0000-0000-0000";
-	result = CCValidator.CheckCard(testCard6);
-	assertEquals("denied", result);
+	result = CCValidator.isValid(testCard6);
+	assertEquals(false, result);
 
 	String testCard7 = "4200-0000-0000-0000";
-	result = CCValidator.CheckCard(testCard7);
-	assertEquals("approved", result);
+	result = CCValidator.isValid(testCard7);
+	assertEquals(true, result);
     }
 }

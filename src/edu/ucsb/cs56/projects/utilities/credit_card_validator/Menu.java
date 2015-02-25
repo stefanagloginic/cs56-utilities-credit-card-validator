aa4a1b4aa96a0e16720e0a40e99ee704d75c407b
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-02-25 15:34:18 -0800

    changed GUI layout, added radio buttons for card types, refactored GUI object names to be more intuitive, added .gitignores at all parts of tree to ignore .DS_Store files

--- src/edu/ucsb/cs56/projects/utilities/credit_card_validator/Menu.java
+++ src/edu/ucsb/cs56/projects/utilities/credit_card_validator/Menu.java
@@ -2,21 +2,32 @@ package edu.ucsb.cs56.projects.utilities.credit_card_validator;
 
 import java.util.Scanner;
 import javax.swing.*;
+import static javax.swing.GroupLayout.Alignment.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
+
+
 public class Menu extends JFrame{
 
 
 	// Textfield where user can enter cardNumberField
 	private JTextField cardNumberField;
+	
 	// User presses this button when ready to validate CC number
-	private JButton validateCardNumber;
+	private JButton validateButton;
 
 	// User presses this button to generate card numbers
-	private JButton generateCardNumber;
+	private JButton generateButton;
 
+	//Simple Label to explain to user what radio buttons do 
 	private JLabel cardTypeLabel;
+
+	//Radio buttons for the different card types we can make
+	JRadioButton visaRadioButton = new JRadioButton("Visa");
+    JRadioButton masterCardRadioButton = new JRadioButton("MasterCard");
+    JRadioButton amexRadioButton = new JRadioButton("Amex");
+    JRadioButton discoverRadioButton = new JRadioButton("Discover");
 
 	// Constructor for Menu calls the initUI() method
 	public Menu() {
@@ -45,6 +56,9 @@ public class Menu extends JFrame{
 		}
 	}
 
+	// TODO: add action listeners for RadioButtones and part in validate to check a box based on card type
+
+
 	// generates a card number and enters it in the bottom text field
 	public void generateCard() {
 		String preText = cardNumberField.getText();
@@ -64,34 +78,80 @@ public class Menu extends JFrame{
 	// creates the text field and buttons and adds them to JFrame
 	public void initUI() {
 		cardNumberField = new JTextField(12);
-		validateCardNumber = new JButton("Validate");
-		generateCardNumber = new JButton("Generate");
-		cardNumberField = new JTextField(12);
+		validateButton = new JButton("Validate");
+		generateButton = new JButton("Generate");
+		visaRadioButton = new JRadioButton("Visa");
+        masterCardRadioButton = new JRadioButton("MasterCard");
+        amexRadioButton = new JRadioButton("Amex");
+        discoverRadioButton = new JRadioButton("Discover");
+        cardTypeLabel = new JLabel("Card Type:");
 		cardNumberField.setText("Enter your credit card number here");
-		cardTypeLabel = new JLabel("Card Type:");
+		
+		
 		this.setTitle("Credit Card Validator");
 		this.setSize(600,100);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		GroupLayout layout = new GroupLayout(this.getContentPane());
+		
+
+ 
+        // remove redundant default border of check boxes - they would hinder
+        // correct spacing and aligning (maybe not needed on some look and feels)
+        visaRadioButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        masterCardRadioButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        amexRadioButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        discoverRadioButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+ 
+        //create new grouplayout and set contentpane to this layout
+        GroupLayout layout = new GroupLayout(this.getContentPane());
     	this.getContentPane().setLayout(layout);
 
-    	layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-		    .addGroup(layout.createSequentialGroup()
-		    	.addComponent(generateCardNumber, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-		        .addComponent(validateCardNumber, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		    .addComponent(cardTypeLabel, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-		    .addComponent(cardNumberField, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+ 
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+            //.addComponent(cardTypeLabel)
+            .addGroup(layout.createParallelGroup(LEADING)
+                .addComponent(cardNumberField)
+            	.addGroup(layout.createSequentialGroup()
+                    .addComponent(cardTypeLabel)
+                    .addGroup(layout.createParallelGroup(LEADING)
+                        .addComponent(visaRadioButton)
+                        .addComponent(amexRadioButton))
+                    .addGroup(layout.createParallelGroup(LEADING)
+                        .addComponent(masterCardRadioButton)
+                        .addComponent(discoverRadioButton))))
+            .addGroup(layout.createParallelGroup(LEADING)
+                .addComponent(validateButton)
+                .addComponent(generateButton))
+        );
+        
+        layout.linkSize(SwingConstants.HORIZONTAL, validateButton, generateButton);
+ 
+        layout.setVerticalGroup(layout.createSequentialGroup()
+            .addGroup(layout.createParallelGroup(BASELINE)
+                //.addComponent(cardTypeLabel)
+                .addComponent(cardNumberField)
+                .addComponent(validateButton))
+            .addGroup(layout.createParallelGroup(LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGroup(layout.createParallelGroup(BASELINE)
+                        .addComponent(cardTypeLabel)
+                        .addComponent(visaRadioButton)
+                        .addComponent(masterCardRadioButton))
+                    .addGroup(layout.createParallelGroup(BASELINE)
+                        .addComponent(amexRadioButton)
+                        .addComponent(discoverRadioButton)))
+                .addComponent(generateButton))
+        );
 
-		layout.setVerticalGroup(layout.createSequentialGroup()
-		    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-		        .addComponent(generateCardNumber).addComponent(validateCardNumber))
-		    .addComponent(cardTypeLabel)
-		    .addComponent(cardNumberField));
-		this.setVisible(true);
-		validateCardNumber.addActionListener(new ValidateListener());
-		generateCardNumber.addActionListener(new GenerateListener());
-		this.repaint();
+        pack();
+     	this.setVisible(true);
+ 		//this.repaint();
+
+		validateButton.addActionListener(new ValidateListener());
+		generateButton.addActionListener(new GenerateListener());
+		
 		
 	}
 
